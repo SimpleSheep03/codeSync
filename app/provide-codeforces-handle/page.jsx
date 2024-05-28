a75a128382f510b942fb43b76e861f937052a992
Sambhav Sinha <sambhavsinha38@gmail.com>
2024-05-28 17:02:47 +0530

    minor bug fixes

--- app/provide-codeforces-handle/page.jsx
+++ app/provide-codeforces-handle/page.jsx
@@ -4,7 +4,6 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
 import Spinner from '@/components/Spinner';
-import Swal from 'sweetalert2'
 
 
 const ProvideCodeforcesHandle = () => {
