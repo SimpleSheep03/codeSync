8ad082b2d52e70bceaa0957d0f4fe25e2e8daaf8
Sambhav Sinha <sambhavsinha38@gmail.com>
2024-08-12 00:13:11 +0530

    improve UI

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -66,7 +66,7 @@ const BarChart = ({ divisions, ratingChange }) => {
   return (
     <div>
       <div id="myDiv2"></div>
-      <p className='px-5 text-center'>This bar chart visualizes the average rating change across different contest divisions excluding the first few contests.</p>
+      <p className='px-5 text-center mt-5'>This bar chart visualizes the average rating change across different contest divisions excluding the first few contests.</p>
 
     </div>
   );
