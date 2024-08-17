7297ebb70dfa40e9a78c36c0f0766ca0ddad1a7b
Sambhav Sinha <sambhavsinha38@gmail.com>
2024-08-18 01:10:00 +0530

    enable speed insights

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AuthProvider from "@/components/AuthProvider";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GlobalProvider } from "@/context/GlobalContext";
 
 export const metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
           <Navbar/>
           {children}
           <Analytics/>
+          <SpeedInsights/>
           <ToastContainer/>
           </body>
       </html>
